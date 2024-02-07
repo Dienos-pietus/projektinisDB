f30842cdf96ce8d09bbb9d7598143afa54c71265
KaLa <k.lapienis@gmail.com>
2024-02-08 00:45:35 +0200

    modal style fixes

--- modules/adminFunctions/addMealModal.js
+++ modules/adminFunctions/addMealModal.js
@@ -1,4 +1,4 @@
-// creates new meal input modal and saves values to the database
+// creates add new meal modal and saves values to the database
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
@@ -12,12 +12,12 @@ export function createMealModal() {
 
     // Create modal container
     const modalContainer = document.createElement("div");
-    modalContainer.classList.add("modal", "fade", "show", "border", "border-warning", "border-2");
+    modalContainer.classList.add("modal", "fade", "show", "border-warning", "border-3");
     modalContainer.id = "mealModalContainer";
     modalContainer.style.display = "block";
     modalContainer.style.backgroundColor = "white";
-    modalContainer.style.width = "400px";
-    modalContainer.style.height = "450px";
+    modalContainer.style.width = "450px";
+    modalContainer.style.height = "420px";
     modalContainer.style.position = "fixed";
     modalContainer.style.top = "50%";
     modalContainer.style.left = "50%";
@@ -25,12 +25,12 @@ export function createMealModal() {
 
     // Create modal dialog
     const modalDialog = document.createElement("div");
-    modalDialog.classList.add("modal-dialog");
+    modalDialog.classList.add("modal-dialog", "my-0", "mx-0");
     modalContainer.append(modalDialog);
 
     // Create modal content
     const modalContent = document.createElement("div");
-    modalContent.classList.add("modal-content");
+    modalContent.classList.add("modal-content", "my-0", "mx-0");
     modalDialog.append(modalContent);
 
     // Create modal header container
@@ -41,8 +41,8 @@ export function createMealModal() {
     // Create modal title
     const modalTitle = document.createElement("h5");
     modalTitle.classList.add("modal-title");
-    modalTitle.id = "mealModalLabel";
     modalTitle.textContent = "Add Meal";
+    modalTitle.style.fontSize = "18px"
     modalHeader.append(modalTitle);
 
     // Create modal body
@@ -55,33 +55,40 @@ export function createMealModal() {
     titleInput.classList.add("form-control");
     titleInput.setAttribute("type", "text");
     titleInput.setAttribute("placeholder", "Meal Title");
+    titleInput.style.fontSize = "15px";
     modalBody.append(titleInput);
 
     const priceInput = document.createElement("input");
     priceInput.classList.add("form-control");
     priceInput.setAttribute("type", "text");
     priceInput.setAttribute("placeholder", "Price");
+    priceInput.style.fontSize = "12px";
     modalBody.append(priceInput);
 
     const descriptionInput = document.createElement("textarea");
     descriptionInput.classList.add("form-control");
     descriptionInput.setAttribute("type", "text");
     descriptionInput.setAttribute("placeholder", "Description");
+    descriptionInput.style.fontSize = "12px";
     modalBody.append(descriptionInput);
 
     const pictureURLInput = document.createElement("input");
     pictureURLInput.classList.add("form-control");
     pictureURLInput.setAttribute("type", "text");
     pictureURLInput.setAttribute("placeholder", "Picture URL");
+    pictureURLInput.style.fontSize = "12px";
     modalBody.append(pictureURLInput);
 
     // Create categories dropdown
     const selectCategory = document.createElement("select");
+    selectCategory.style.fontSize = "12px";
     selectCategory.classList.add("form-control");
-    const categories = ["breakfast", "lunch", "dinner", "shakes"];
+    
+    const categories = ["Breakfast", "Lunch", "Dinner", "Shakes"];
     categories.forEach(category => {
         const option = document.createElement("option");
         option.value = category;
+        option.style.fontSize = "12px";
         option.textContent = category;
         selectCategory.append(option);
     });
